4f3211f5949b4914baa8f7fca6b931424cc317da
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-07-18 14:54:39 -0300

    Service layer, component registration

--- src/main/java/com/javaspring/curso/resources/UserResource.java
+++ src/main/java/com/javaspring/curso/resources/UserResource.java
@@ -1,20 +1,37 @@
 package com.javaspring.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaspring.curso.entities.User;
+import com.javaspring.curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-  @GetMapping
-  public ResponseEntity<User> findAll() {
-    User u = new User(1L, "Danilo", "danilo@email.com", "999999999", "pass");
+  @Autowired
+  private UserService service;
 
-    return ResponseEntity.ok().body(u);
+  @GetMapping
+  public ResponseEntity<List<User>> findAll() {
+    List<User> list = service.findAll();
+
+    return ResponseEntity.ok().body(list);
   }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<User> findById(@PathVariable Long id) {
+    User user = service.findById(id);
+
+    return ResponseEntity.ok().body(user);
+  }
+
+
 }
