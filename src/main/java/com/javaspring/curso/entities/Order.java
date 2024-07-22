dc11869a9449dbfbc898437739b820b5d14b54db
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-07-22 16:18:57 -0300

    subtotal and total methods

--- src/main/java/com/javaspring/curso/entities/Order.java
+++ src/main/java/com/javaspring/curso/entities/Order.java
@@ -99,6 +99,15 @@ public class Order implements Serializable {
     this.payment = payment;
   }
 
+  public Double getTotal() {
+    Double sum = 0.0;
+    for (OrderItem orderItem : items) {
+      sum += orderItem.getSubTotal();
+    }
+
+    return sum;
+  }
+
   @Override
   public int hashCode() {
     final int prime = 31;
